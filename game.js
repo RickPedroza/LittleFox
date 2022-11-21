542aef45e0d162434ea9820f6d4dd7fc736c86e2
RickPedroza <rpedroza0208@gmail.com>
2022-11-21 11:35:55 -0300

    normal commit

--- game.js
+++ game.js
@@ -4,7 +4,13 @@ var config =
     width: 1475,
     height: 725,
     backgroundColor: 0x261321,
-    scene: [Scene1, Scene2]
+    scene: [Scene1, Scene2],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+        }
+    }
 } 
 //TAMANHO DA TELA DO JOGO: 270
 var game = new Phaser.Game(config);
--- slash.js
+++ slash.js
@@ -6,5 +6,16 @@ class Slash extends Phaser.GameObjects.Sprite{
 
         super(scene, x, y, "slash");
         
+        // 3.2 add to scene
+        scene.add.existing(this);
+
+        // 3.3
+        this.play("slash_anim");
+        scene.physics.world.enableBody(this);
+        this.body.velocity.x += 250;
+
+
+        // 4.2 add the beam to the projectiles group
+        scene.projectiles.add(this);
     }
 }
